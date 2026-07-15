31df17f0f5cc64b114b47a1228b09f7aa3aa4ad7
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-15 22:07:20 +0530

    Update Square.jsx

--- components/Square.jsx
+++ components/Square.jsx
@@ -1,78 +1,101 @@
-function Square({
+function Timer({
 
-    piece,
+    whiteTime,
 
-    row,
+    blackTime,
 
-    col,
-
-    selected,
-
-    legalMove,
-
-    onClick
+    turn
 
 }) {
 
 
-    function handleClick(){
 
-        onClick(row,col);
+    function formatTime(seconds){
+
+
+        const minutes =
+            Math.floor(seconds / 60);
+
+
+        const secondsLeft =
+            seconds % 60;
+
+
+
+        return (
+
+            `${String(minutes).padStart(2,"0")}:${String(secondsLeft).padStart(2,"0")}`
+
+        );
+
 
     }
 
 
 
-    const squareColor =
-        (row + col) % 2 === 0
-        ? "light"
-        : "dark";
-
 
 
     return (
 
-        <div
-
-            className={
-
-                `square
-
-                ${squareColor}
-
-                ${selected ? "selected" : ""}
-
-                ${legalMove ? "legal" : ""}
-
-                `
-
-            }
-
-
-            onClick={handleClick}
-
-        >
-
-
-            {
-                piece !== "" &&
-
-                <span className="piece">
-
-                    {getPieceImage(piece)}
-
-                </span>
-
-            }
+        <div className="timer">
 
 
 
-            {
-                legalMove &&
+            <div
 
-                <div className="move-dot"></div>
+                className={
 
-            }
+                    turn === "white"
+
+                    ? "time-box active-time"
+
+                    : "time-box"
+
+                }
+
+            >
+
+                <h3>
+                    White
+                </h3>
+
+
+                <p>
+                    {formatTime(whiteTime)}
+                </p>
+
+
+            </div>
+
+
+
+
+
+            <div
+
+                className={
+
+                    turn === "black"
+
+                    ? "time-box active-time"
+
+                    : "time-box"
+
+                }
+
+            >
+
+                <h3>
+                    Black
+                </h3>
+
+
+                <p>
+                    {formatTime(blackTime)}
+                </p>
+
+
+            </div>
 
 
 
@@ -84,49 +107,4 @@ function Square({
 
 
 
-
-
-function getPieceImage(piece){
-
-
-    const pieces = {
-
-
-        wp:"♙",
-
-        wr:"♖",
-
-        wn:"♘",
-
-        wb:"♗",
-
-        wq:"♕",
-
-        wk:"♔",
-
-
-
-        bp:"♟",
-
-        br:"♜",
-
-        bn:"♞",
-
-        bb:"♝",
-
-        bq:"♛",
-
-        bk:"♚"
-
-
-    };
-
-
-
-    return pieces[piece] || "";
-
-}
-
-
-
-export default Square;
+export default Timer;
