ff0320a19eaa6b2d016ccf88156d6921b476debf
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-16 18:22:13 +0530

    Update Square.jsx

--- components/Square.jsx
+++ components/Square.jsx
@@ -1,81 +1,30 @@
 function Square({
-
-    piece,
-    row,
-    col,
-    selected,
-    legalMove,
-    onClick
-
+  piece,
+  row,
+  col,
+  isSelected,
+  onClick,
 }) {
 
+  // Light and dark square colors
+  const isLight = (row + col) % 2 === 0;
 
-    return (
-
-        <div
-
-            className={
-                `square 
-                ${(row + col) % 2 === 0 ? "light" : "dark"}
-                ${selected ? "selected" : ""}
-                ${legalMove ? "legal" : ""}`
-            }
-
-            onClick={() => onClick(row,col)}
-
-        >
-
-            {
-                piece &&
-                <span className="piece">
-
-                    {getPieceImage(piece)}
-
-                </span>
-            }
-
-
-            {
-                legalMove &&
-                <div className="move-dot"></div>
-            }
-
-
-        </div>
-
-    );
-
+  return (
+    <div
+      className={`square
+        ${isLight ? "light-square" : "dark-square"}
+        ${isSelected ? "selected-square" : ""}
+      `}
+      onClick={() => onClick(row, col)}
+    >
+      {/* Show piece if square is not empty */}
+      {piece && (
+        <span className="piece">
+          {piece.symbol}
+        </span>
+      )}
+    </div>
+  );
 }
-
-
-
-
-function getPieceImage(piece){
-
-
-    const pieces={
-
-        wp:"♙",
-        wr:"♖",
-        wn:"♘",
-        wb:"♗",
-        wq:"♕",
-        wk:"♔",
-
-        bp:"♟",
-        br:"♜",
-        bn:"♞",
-        bb:"♝",
-        bq:"♛",
-        bk:"♚"
-
-    };
-
-
-    return pieces[piece] || "";
-
-}
-
-
 
 export default Square;
