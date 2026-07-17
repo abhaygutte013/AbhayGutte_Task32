28343dfc645d6a78d4067b5d9529edf7d448b1e9
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-18 00:34:59 +0530

    Refactor isPathClear function formatting

--- utils/pathCheck.js
+++ utils/pathCheck.js
@@ -1,49 +1,25 @@
 function isPathClear(
-
     board,
-
     fromRow,
-
     fromCol,
-
     toRow,
-
     toCol
-
 ) {
-
     const rowStep = Math.sign(toRow - fromRow);
-
     const colStep = Math.sign(toCol - fromCol);
-
     let currentRow = fromRow + rowStep;
-
     let currentCol = fromCol + colStep;
-
     // Check every square until the destination
-
     while (
-
         currentRow !== toRow ||
-
         currentCol !== toCol
-
     ) {
-
         if (board[currentRow][currentCol] !== "") {
-
             return false;
-
         }
-
         currentRow += rowStep;
-
         currentCol += colStep;
-
     }
-
     return true;
-
 }
-
 export default isPathClear;
