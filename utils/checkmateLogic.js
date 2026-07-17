cab884324d61646fd4ba7c3b2fff6db9216e3670
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-18 00:33:48 +0530

    Update checkmateLogic.js

--- utils/checkmateLogic.js
+++ utils/checkmateLogic.js
@@ -1,74 +1,72 @@
 import isValidMove from "/utils/pieceMoves.js";
 import { isKingInCheck } from "/utils/checkLogic.js";
 
-// Make a copy of the board
 function copyBoard(board) {
   return board.map((row) =>
-    row.map((piece) => {
-      if (piece === null) return null;
-      return { ...piece };
-    })
+    row.map((piece) =>
+      piece ? { ...piece } : null
+    )
   );
 }
-
-function isCheckmate(board, color) {
-
-  // If king is not in check, it can't be checkmate
-  if (!isKingInCheck(board, color)) {
+export default function isCheckmate(board, currentPlayer) {
+  if (!isKingInCheck(board, currentPlayer)) {
     return false;
   }
-
-  // Try every possible move for every piece
   for (let fromRow = 0; fromRow < 8; fromRow++) {
-
     for (let fromCol = 0; fromCol < 8; fromCol++) {
-
       const piece = board[fromRow][fromCol];
-
-      if (!piece) {
-        continue;
-      }
-
-      if (piece.color !== color) {
-        continue;
-      }
-
-      // Try moving to every square
+      if (!piece) continue;
+      if (piece.color !== currentPlayer) continue;
       for (let toRow = 0; toRow < 8; toRow++) {
-
         for (let toCol = 0; toCol < 8; toCol++) {
-
-          // Skip illegal moves
           if (
             !isValidMove(
               board,
               fromRow,
               fromCol,
               toRow,
-              toCol
+              toCol,
+              currentPlayer
             )
           ) {
             continue;
           }
-
-          // Create a temporary board
           const newBoard = copyBoard(board);
-
-          // Make the move
-          newBoard[toRow][toCol] = newBoard[fromRow][fromCol];
+          newBoard[toRow][toCol] = {
+            ...newBoard[fromRow][fromCol],
+          };
           newBoard[fromRow][fromCol] = null;
-
-          // Is the king still in check?
-          if (!isKingInCheck(newBoard, color)) {
+          if (
+            piece.type === "king" &&
+            Math.abs(toCol - fromCol) === 2
+          ) {
+            if (toCol > fromCol) {
+              newBoard[fromRow][5] = newBoard[fromRow][7];
+              newBoard[fromRow][7] = null;
+            }
+            else {
+              newBoard[fromRow][3] = newBoard[fromRow][0];
+              newBoard[fromRow][0] = null;
+            }
+          }
+          if (
+            piece.type === "pawn" &&
+            Math.abs(toCol - fromCol) === 1 &&
+            board[toRow][toCol] === null
+          ) {
+            newBoard[fromRow][toCol] = null;
+          }
+          if (
+            !isKingInCheck(
+              newBoard,
+              currentPlayer
+            )
+          ) {
             return false;
           }
         }
       }
     }
   }
-
-  // No move can save the king
   return true;
 }
-
-export default isCheckmate;
