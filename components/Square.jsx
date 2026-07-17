757dc58e3a9b74cf59ad77336398761ef40fe014
Abhay Gutte <abhaygutte292@gmail.com>
2026-07-18 00:31:36 +0530

    Update Square.jsx

--- components/Square.jsx
+++ components/Square.jsx
@@ -3,28 +3,40 @@ function Square({
   row,
   col,
   isSelected,
+  isLegalMove,
+  isLastMove,
   onClick,
 }) {
 
-  // Light and dark square colors
   const isLight = (row + col) % 2 === 0;
+  let squareClass = "square ";
+  squareClass += isLight
+    ? "light "
+    : "dark ";
+  if (isSelected) {
+    squareClass += "selected ";
+  }
+  if (isLegalMove) {
+    squareClass += "legal-move ";
+  }
+  if (isLastMove) {
+    squareClass += "last-move ";
+  }
 
   return (
     <div
-      className={`square
-        ${isLight ? "light-square" : "dark-square"}
-        ${isSelected ? "selected-square" : ""}
-      `}
+      className={squareClass}
       onClick={() => onClick(row, col)}
     >
-      {/* Show piece if square is not empty */}
       {piece && (
         <span className="piece">
           {piece.symbol}
         </span>
       )}
+      {!piece && isLegalMove && (
+        <div className="legal-dot"></div>
+      )}
     </div>
   );
 }
-
 export default Square;
